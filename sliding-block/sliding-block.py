04e3cf99e9f0ed3e6bf22b87d1bcc3eabf8e5365
Rachel Dorn <rachel.m.dorn@gmail.com>
2018-05-12 10:20:59 -0400

    create random initial position

--- sliding-block/sliding-block.py
+++ sliding-block/sliding-block.py
@@ -1,7 +1,14 @@
+from random import shuffle
+
 def printBoard(positions):
     print ("""
     | %s | %s | %s |
     | %s | %s | %s |
     | %s | %s | %s |""" % tuple(positions))
 
-printBoard([1, 2, 3, 4, 5, 6, 7, 8, "_"])
+def setInitialPosition():
+    initialElements = [1, 2, 3, 4, 5, 6, 7, 8, "_"]
+    shuffle(initialElements)
+    return initialElements
+
+printBoard(setInitialPosition())
